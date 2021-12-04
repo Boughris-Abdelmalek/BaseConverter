240deacb7cb8eff1a3100494a27e30707c21fdba
mfurkanerten <95361085+mfurkanerten@users.noreply.github.com>
2021-12-04 12:16:21 +0000

    add selectors

---
+++ script.js
@@ -0,0 +1,53 @@
+
+var firstInput = document.querySelector("#xx");
+var value1 = document.querySelector("#yy");
+var value2 = document.querySelector("#zz");
+var convButton = document.getElementsByClassName("convert");
+var resetButton = document.getElementsByClassName("reset");
+var result = document.getElementById("res");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
